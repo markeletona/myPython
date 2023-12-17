01ef0183bd2bcec410c564dbc806738b7cc596de
markel <41082643+markeletona@users.noreply.github.com>
2023-12-17 21:59:08 +0100

    bug fixes
    Bug fixes including properly treating transliteration exceptions involving positions of characters within words when those words are found within larger sentences.
    Also, and related, changed the way how punctuations marks are treated. Still need to add capability to handle punctuation characters within words (e.g., "-")

--- translit_ru_es.py
+++ translit_ru_es.py
@@ -10,6 +10,13 @@ Oficina de las Naciones Unidas en Ginebra.
 https://ls-sts.unog.ch/section-files/normas-transliteracion-ru-es
 
 @author: Usuario
+
+
+## TO BE DONE:
+    - CONSIDER ACCENTS (THEY MIGHT BE PRESENT IN DICTIONARIES ETC. FOR AID)
+    - IF MORE THAN TWO WORDS ARE INTRODUCED, WORDS NEED TO BE SPLIT SO THAT 
+      POSITIONAL CONDITIONS ARE CORRECTLY ADDRESSED (BEGINNINGS/ENDS OF WORDS ETC.)
+      
 """
 
 #%% THE FUNCTION
@@ -105,99 +112,143 @@ def translit_ru_es(s, check_all_upper=True):
         'Ю': 'Yu',
         'ю': 'yu',
         'Я': 'Ya',
-        'я': 'ya',
-        ' ': ' ', # espacio
-        '.': '.', # signos de puntuacion para permitir translit. de frases
-        ',': ',',
-        '?': '?',
-        '!': '!',
-        ':': ':',
-        ';': ';'
+        'я': 'ya'
         }
-    
-    upper_list = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К',
-                  'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц',
-                  'Ч', 'Ш', 'Щ', 'ъ', 'Ы', 'ь', 'Э', 'Ю', 'Я']
     
     # * "There are no native Russian words that begin with ⟨ы⟩ (except for the 
     # specific verb ыкать: "to say the ⟨ы⟩-sound"), but there are many proper 
     # and common nouns of non-Russian origin (including some geographical names
     # in Russia) that begin with it." -> en.wikipedia.org/wiki/Yery
     
+    upper_list = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К',
+                  'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц',
+                  'Ч', 'Ш', 'Щ', 'ъ', 'Ы', 'ь', 'Э', 'Ю', 'Я']
+    
+    # signos de puntuacion para permitir translit. de frases
+    punc = {'.': '.', 
+            ',': ',',
+            '?': '?',
+            '!': '!',
+            ':': ':',
+            ';': ';',
+            '-': '-',
+            '–': '–',
+            '—': '—',
+            '"': '"'}
+
+    
+    # Try to coerce input to string class as safeguard
+    s = str(s)
+    
+    # Split words in phrase
+    ss = s.split(" ")
     
     out = ""
-    for i, v in enumerate(s):
+    for iw, w in enumerate(ss):
         
-        # Check if s is a string
-        if not s.__class__==str:
-            raise ValueError('Input needs to be a string.')
-        
-        # Realizar transliteración básica:
-        bt = basic_translit[v]
-        
-        # Considerar excepciones:
-        
-        # INCLUYE CONDICIONES PARA ASEGURAR QUE s[i+1], s[i-1] no se salgan de 
-        # la palabra:
-        # - Para s[i+1] tiene que darse que i<(len(s)-1) [i empieza de 0, len() 
-        #   cuenta desde 1]
-        # - Para s[i-1] tiene que darse que i>0
-        
-        # Гг -> gu — delante de "e" y "и":
-        if (v=='Г') & (i<(len(s)-1)):
-            if s[i+1] in ['е', 'и']:
-                bt = 'Gu'
-        if (v=='г') & (i<(len(s)-1)):
-            if s[i+1] in ['е', 'и']:
-                bt = 'gu'
+         #[ Remove punctuation marks to properly treat exceptions to end of
+         # word etc. but how do i put then back??]
+        w_copy = w # save original word
+        ps = {}
+        for ip, p in enumerate(punc):
+            if p in w:
+                # record the position the puncuation mark was in
+                ps[p] = w.find(p)
+                # remove it
+                w = w.replace(p,'')
                 
-        # Ее -> ye — inicial o tras vocal, "ъ" o "ь":
-        if (v=='Е') & (i==0):
-            bt = 'Ye'
-        if (v=='Е') & (i>0):
-            if s[i-1] in ['А', 'Я', 'Э', 'Е', 'Ы', 'И', 'О', 'Ё', 'У', 'Ю', 'ъ', 'ь']:
-                bt = 'Ye'
-        if (v=='е') & (i==0):
-            bt = 'ye'
-        if (v=='е') & (i>0):
-            if s[i-1] in  ['а', 'я', 'э', 'е', 'ы', 'и', 'о', 'ё', 'у', 'ю', 'ъ', 'ь']:
-                bt = 'ye'        
-        
-        # Ёё -> o — tras "ж", "ч", "ш" o "щ":
-        if (v=='Ё') & (i>0):
-            if s[i-1] in ['Ж', 'Ч', 'Ш', 'Щ']:
-                bt = 'O'
-        if (v=='ё') & (i>0):
-            if s[i-1] in ['ж', 'ч', 'ш', 'щ']:
-                bt = 'o'
-                
-        # Йй -> (se omite tras "y" (ы) u otra "i") | y – tras "a", "e", "o", "у" al final de palabra (ESTO DEBE REFERIRSE AL CIRILICO (y=u), SI NO NO TENDRÍA SENTIDO "y" después de "y")
-        if (v=='Й') & (i>0):
-            if out[-1] in ['Y', 'y', 'I', 'i']:
-                bt = ''
-            elif ((i==(len(s)-1)) & (s[i-1] in ['А', 'а', 'Е', 'е', 'О', 'о', 'У', 'у'])): 
-                bt = 'Y'
-        if (v=='й') & (i>0):
-            if out[-1] in ['Y', 'y', 'I', 'i']:
-                bt = ''
-            elif ((i==(len(s)-1)) & (s[i-1] in ['а', 'е', 'о', 'у'])):
-                bt = 'y'
+        w = "уезд" #vs Уезд
+        w = "Уезд"
+        out = ""
+        for i, v in enumerate(w):
             
-        # Яя -> a – tras “и” al final de palabra:
-        if (v=='Я') & (i==(len(s)-1)) & (s[i-1]=='И'):
-            bt='A'
-        if (v=='я') & (i==(len(s)-1)) & (s[i-1]=='и'):
-            bt='a'
+            # Realizar transliteración básica:
+            bt = basic_translit[v]
+            
+            # Considerar excepciones:
+            
+            # INCLUYE CONDICIONES PARA ASEGURAR QUE w[i+1], w[i-1] no se salgan de 
+            # la palabra:
+            # - Para w[i+1] tiene que darse que i<(len(w)-1) [i empieza de 0, len() 
+            #   cuenta desde 1]
+            # - Para w[i-1] tiene que darse que i>0
+            
+            # Гг -> gu — delante de "e" y "и":
+            if (v=='Г') & (i<(len(w)-1)):
+                if w[i+1] in ['е', 'и']:
+                    bt = 'Gu'
+            if (v=='г') & (i<(len(w)-1)):
+                if w[i+1] in ['е', 'и']:
+                    bt = 'gu'
+                    
+            # Ее -> ye — inicial o tras vocal, "ъ" o "ь":
+            if (v=='Е') & (i==0):
+                bt = 'Ye'
+            if (v=='Е') & (i>0):
+                if w[i-1] in ['а', 'я', 'э', 'е', 'ы', 'и', 'о', 'ё', 'у', 'ю',
+                              'А', 'Я', 'Э', 'Е', 'Ы', 'И', 'О', 'Ё', 'У', 'Ю',
+                              'ъ', 'ь']:
+                    bt = 'Ye'
+            if (v=='е') & (i==0):
+                bt = 'ye'
+            if (v=='е') & (i>0):
+                if w[i-1] in ['а', 'я', 'э', 'е', 'ы', 'и', 'о', 'ё', 'у', 'ю',
+                              'А', 'Я', 'Э', 'Е', 'Ы', 'И', 'О', 'Ё', 'У', 'Ю',
+                              'ъ', 'ь']:
+                    bt = 'ye'        
+            
+            # Ёё -> o — tras "ж", "ч", "ш" o "щ":
+            if (v=='Ё') & (i>0):
+                if w[i-1] in ['Ж', 'Ч', 'Ш', 'Щ']:
+                    bt = 'O'
+            if (v=='ё') & (i>0):
+                if w[i-1] in ['ж', 'ч', 'ш', 'щ']:
+                    bt = 'o'
+                    
+            # Йй -> (se omite tras "y" (ы) u otra "i") | y – tras "a", "e", "o", "у" al final de palabra (ESTO DEBE REFERIRSE AL CIRILICO (y=u), SI NO NO TENDRÍA SENTIDO "y" después de "y")
+            if (v=='Й') & (i>0):
+                if out[-1] in ['Y', 'y', 'I', 'i']:
+                    bt = ''
+                elif ((i==(len(w)-1)) & (w[i-1] in ['А', 'а', 'Е', 'е', 'О', 'о', 'У', 'у'])): 
+                    bt = 'Y'
+            if (v=='й') & (i>0):
+                if out[-1] in ['Y', 'y', 'I', 'i']:
+                    bt = ''
+                elif ((i==(len(w)-1)) & (w[i-1] in ['а', 'е', 'о', 'у'])):
+                    bt = 'y'
+                
+            # Яя -> a – tras “и” al final de palabra:
+            if (v=='Я') & (i==(len(w)-1)) & (w[i-1]=='И'):
+                bt='A'
+            if (v=='я') & (i==(len(w)-1)) & (w[i-1]=='и'):
+                bt='a'
+            
+            # Unir con anterior:     
+            out = out + bt
         
-        # Unir con anterior:     
-        out = out + bt
+        # After finishing each word
+        # 1) add back punctuation marks
+        for p in ps:
+            # the translirated word could have a different no. of characters
+            # even with that, easy to substitute chars at start and end
+            if ps[p]==0: # start
+                out = p + out
+            elif ps[p]==(len(w_copy)-1):
+                out = out + p
+                
+            # but what about chars in between, say composed words like "Askania-Nova", if the output has less/more characters how do I locate where to put it? the original position may not match the new one
+            # oh shit i can't do it after the word is finished if it's in the middle
+            
+        # 2) add white space (except for last)
+        if not iw==(len(ss)-1): out = out + " "
+
     
     # Chequear si toda la palabra en ruso son mayúsculas para hacer lo propio
     # con la transliteración:
     if check_all_upper:
         # primero quitar signos de puntuación antes de chequear mayus:
         s2 = s
-        chars = " .,?!:;"
+        chars = ''.join(list(punc.values()))
         for c in chars:
             s2 = s2.replace(c, '')
         # comprobar mayúsculas:
@@ -248,3 +299,8 @@ else:
 # Otros ejemplos:
 # test_text = u"Всероссийский Центральный Исполнительный Комитет"
 # translit_ru_es(test_text, check_all_upper=False)
+
+#%%
+
+
+translit_ru_es("Варвара Николаевна, Яковлева.")
